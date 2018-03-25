664fa53924527f4bba654702484322d4477b4e09
thealexvarga <thealexvarga@gmail.com>
2018-03-25 15:16:43 -0400

    outer loops

--- pemdice.py
+++ pemdice.py
@@ -1,17 +1,19 @@
 
 from math import sqrt
+# from copy import copy, deepcopy
 
 # list of shape groups
-AS, NEG, MD, RT, MID, LF, JUX, PAR, CON, INT, FRA = range(11)
+EQ, AS, NEG, MD, RT, MID, LF, JUX, PAR, CON, INT, FRA = range(12)
 
-Arity = [2, 1, 2, 1, 2, 1, 2, 1, 2, 0]
+Arity = [2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0, 0]
 
 DICE = {
-	'+': AS, '-': AS,
+	'-': NEG,
+	'+': AS, #'-': AS,
 	'*': MD, '/': MD,
-	'^2': RT, '^3': RT, '!': RT, '**2': RT, '**3': RT,
-	'^': MID, '**': MID,
-	'sqrt': LF, 'cbrt': RT,
+	'^2': RT, '^3': RT, '!': RT, #'**2': RT, '**3': RT,
+	'^': MID, #'**': MID,
+	'sqrt': LF, 'cbrt': LF,
 	'1/2': FRA, '1/3': FRA, '1/4': FRA,
 }
 for i in '2,3,4,5,6,7,8,9,10,20,30,40,50,60,100'.split(','):
@@ -31,16 +33,41 @@ def S(tup):
 	if op is CON: 	return S(rest[0]) + S(rest[1])
 	# if op is INT: return '?'
 
+def test(C): return C[INT]+C[FRA]-C[CON]-C[JUX] == C[AS]+C[MD]+C[EQ]+1
+
 # def parseDice(dice):
 # 	return map(lambda x: (DICE[x], x), dice.split(', '))
 
 
-t = (AS, (MD, (PAR, (AS, INT, (LF, (CON, INT, INT)))), (NEG, INT)), (RT, INT))
-print(S(t))
-print(eval(S(t)))
+# t = (AS, (MD, (PAR, (AS, INT, (LF, (CON, INT, INT)))), (NEG, INT)), (RT, INT))
+# print(S(t))
+# print(eval(S(t)))
 
-dice = "2, 3, /, 1/3, 8, *, cbrt, +, ^3, -, 3, 30".split(', ')
-print(dice.count('-') > dice.count('^'))
+# dice = "2 3 / 1/3 8 * cbrt - ^ - 3 30".split()
+# dice = "20 - 2 ^2 1/2 - * 5 9 5 + ^2".split()
+dice = "4 10 8 3 2 + * *".split()
+COUNTS = [0 for _ in range(len(Arity))]
+for d in dice: COUNTS[DICE[d]] += 1
+COUNTS[PAR] = 1
+COUNTS[EQ] = 1
+
+negsAndSubs = COUNTS[NEG]
+exps = COUNTS[MID]
+diff = negsAndSubs-exps
+for negs in range(diff+1):
+	subs = negsAndSubs - negs
+	consAndJuxes = negs
+	for cons in range(consAndJuxes+1):
+		juxes = consAndJuxes - cons
+		counts = list(COUNTS)
+		counts[NEG] = negs
+		counts[AS] += subs
+		counts[CON] = cons
+		counts[JUX] = juxes
+		print(counts)
+		print(test(counts))
+		# print(negs, subs, exps, cons, juxes)
+
 
 
 
