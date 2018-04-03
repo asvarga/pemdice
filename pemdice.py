73cc0135755dc85e028db59b07b344a955c64148
thealexvarga <thealexvarga@gmail.com>
2018-04-02 22:24:25 -0400

    some simplification; notes

--- pemdice.py
+++ pemdice.py
@@ -3,26 +3,46 @@ from math import sqrt
 # from copy import copy, deepcopy
 
 # list of shape groups
-EQ, AS, NEG, MD, RT, MID, LF, JUX, PAR, CON, INT, FRA = range(12)
+# EQ, AS, NEG, MD, RT, MID, LF, JUX, PAR, CON, INT, FRA = range(12)
 
-Arity = [2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0, 0]
+# Arity = [2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0, 0]
 
-LT, EQ, GT = -1, 0, 1
+# LE, EQ, GE = -1, 0, 1
+
+# LIMS = [
+# 	[(GE, EQ), (GE, EQ)],
+# 	[(GE, AS), (GE, MD)],
+# 	[(GE, MD)],
+# 	[(GE, MD), (GE, RT)],
+# 	[(GE, RT)],
+# 	[(GE, MID), (GE, LF)],
+# 	[(GE, LF)],
+# 	[(GE, PAR), (EQ, PAR)],
+# 	[PAR], ## more than in
+# 	[(EQ, INT), (GE, PAR)],
+# 	[],
+# 	[]
+# ]
+
+
+numOps = 11
+AS, NEG, MD, RT, MID, LF, NUM,    EQ, JUX, INT, FRA = range(numOps)
+
+Arity = [2, 1, 2, 1, 2, 1, 0]
+
+LE, EQ, GE = -1, 0, 1
 
 LIMS = [
-	[(GT, EQ), (GT, EQ)],
-	[(GT, AS), (GT, NEG)],
-	[(GT, NEG)],
-	[(GT, MD), (GT, MD)],
-	[(GT, RT)],
-	[(GT, MID), (GT, MID)],
-	[(GT, LF)],
-	[(GT, PAR), (EQ, PAR)],
-	[(PAR)], ## more than in
-	[(EQ, INT), (GT, PAR)],
-	[],
+	[(GE, AS), (GE, MD)],
+	[(GE, MD)],
+	[(GE, MD), (GE, RT)],
+	[(GE, RT)],
+	[(GE, MID), (GE, LF)],
+	[(GE, LF)],
 	[]
 ]
+
+
 
 DICE = {
 	'-': NEG,
@@ -37,7 +57,7 @@ for i in '2,3,4,5,6,7,8,9,10,20,30,40,50,60,100'.split(','):
 	DICE[i] = INT
 
 def S(tup):
-	if tup is INT: return '3'
+	if tup is NUM: return '3'
 	op, rest = tup[0], tup[1:]
 	if op is AS: 	return S(rest[0]) + '+' + S(rest[1])
 	if op is NEG: 	return '-' + S(rest[0])
@@ -47,7 +67,7 @@ def S(tup):
 	if op is LF: 	return 'sqrt(' + S(rest[0]) + ')'
 	if op is JUX: 	return S(rest[0]) + S(rest[1])
 	if op is PAR: 	return '(' + S(rest[0]) + ')'
-	if op is CON: 	return S(rest[0]) + S(rest[1])
+	# if op is CON: 	return S(rest[0]) + S(rest[1])
 	# if op is INT: return '?'
 
 def test(C): return C[INT]+C[FRA]-C[CON]-C[JUX] == C[AS]+C[MD]+C[EQ]+1
@@ -61,12 +81,20 @@ def test(C): return C[INT]+C[FRA]-C[CON]-C[JUX] == C[AS]+C[MD]+C[EQ]+1
 # print(eval(S(t)))
 
 # dice = "2 3 / 1/3 8 * cbrt - ^ - 3 30".split()
-# dice = "20 - 2 ^2 1/2 - * 5 9 5 + ^2".split()
-dice = "4 10 8 3 2 + * *".split()
-COUNTS = [0 for _ in range(len(Arity))]
+dice = "20 - 2 ^2 1/2 - * 5 9 5 + ^2".split()
+# dice = "4 10 8 3 2 + * *".split()
+COUNTS = [0 for _ in range(numOps)]
 for d in dice: COUNTS[DICE[d]] += 1
-COUNTS[PAR] = 1
-COUNTS[EQ] = 1
+# COUNTS[PAR] = 1
+# COUNTS[EQ] = 1
+ints = list(filter(lambda d: DICE[d] is INT, dice))
+fra = list(filter(lambda d: DICE[d] is FRA, dice))[0]
+print(ints)
+print(fra)
+
+# hardcoded sets of numbers based on number of concats available:
+numSets = [[ints+[fra]], [], []]
+print(numSets)
 
 negsAndSubs = COUNTS[NEG]
 exps = COUNTS[MID]
@@ -74,19 +102,17 @@ diff = negsAndSubs-exps
 for negs in range(diff+1):
 	subs = negsAndSubs - negs
 	consAndJuxes = negs
-	for cons in range(consAndJuxes+1):
-		juxes = consAndJuxes - cons
-		counts = list(COUNTS)
-		counts[NEG] = negs
-		counts[AS] += subs
-		counts[CON] = cons
-		counts[JUX] = juxes
-		print(counts)
-		print(test(counts))
-		# print(negs, subs, exps, cons, juxes)
-
-
-
+	for juxes in range(min(consAndJuxes+1, 2)):
+		cons = consAndJuxes - juxes
+		print(negs, subs, exps, cons, juxes)
+		# counts = list(COUNTS)
+		# counts[NEG] = negs
+		# counts[AS] += subs
+		# counts[CON] = cons
+		# counts[JUX] = juxes
+		# print(counts)
+		# print(test(counts))
+		
 
 
 
